f54d5c1d741be980e2fa17321ed4091d73453613
Mykhailo Kurov <miklay89@gmail.com>
2023-06-14 13:47:53 +0100

    fix deploy bug

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import PrivateRoute from "./PrivateRouteComponent";
 import HomePage from "../components/pages/home/Home";
 import Account from "../components/pages/account/Account";
-import Paywall from "../components/pages/paywall/Paywall";
 import Chat from "../components/pages/chat/Chat";
 
 export enum Paths {
